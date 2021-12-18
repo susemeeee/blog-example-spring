03cf73788642745260488599a644b498c65c1545
susemeeee <hffg123@naver.com>
2021-12-18 10:50:40 +0900

    Initial commit

---
+++ example-spring-security/src/main/java/com/example/examplespringsecurity/ExampleSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ example-spring-security/src/test/java/com/example/examplespringsecurity/ExampleSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examplespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
