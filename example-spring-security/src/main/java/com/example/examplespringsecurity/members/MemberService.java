09476e2bcc0dd614b0d9599583d8b080b60e4c62
susemeeee <hffg123@naver.com>
2021-12-19 10:51:11 +0900

    Add MemberService

---
+++ example-spring-security/src/main/java/com/example/examplespringsecurity/members/MemberService.java
@@ -0,0 +1,24 @@
+/*
+ * MemberService.java
+ * Author : 박찬형
+ * Created Date : 2021-12-18
+ */
+package com.example.examplespringsecurity.members;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found."));
+    }
+}
