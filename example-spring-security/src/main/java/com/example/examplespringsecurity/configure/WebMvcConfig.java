6d0c2be26f2a436912a6fc4f42e3cf34528342d8
susemeeee <hffg123@naver.com>
2021-12-19 10:51:31 +0900

    Add WebMvcConfig

---
+++ example-spring-security/src/main/java/com/example/examplespringsecurity/configure/WebMvcConfig.java
@@ -0,0 +1,19 @@
+/*
+ * WebMvcConfig.java
+ * Author : 박찬형
+ * Created Date : 2021-12-18
+ */
+package com.example.examplespringsecurity.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/success").setViewName("success");
+        registry.addViewController("/admin").setViewName("admin");
+    }
+}
