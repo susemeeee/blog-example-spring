bb0bcc9fae1b08c99b26907d60d1e8df8513cd45
susemeeee <hffg123@naver.com>
2021-12-19 10:51:48 +0900

    Add WebSecurityConfig

---
+++ example-spring-security/src/main/java/com/example/examplespringsecurity/configure/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+/*
+ * WebSecurityConfig.java
+ * Author : 박찬형
+ * Created Date : 2021-12-18
+ */
+package com.example.examplespringsecurity.configure;
+
+import com.example.examplespringsecurity.members.MemberService;
+import com.example.examplespringsecurity.members.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final MemberService memberService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/templates/**", "/static/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .formLogin()
+                .defaultSuccessUrl("/success")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/success").hasAnyRole(Role.USER.name(), Role.ADMIN.name())
+                .antMatchers("/admin").hasRole(Role.ADMIN.name())
+                .anyRequest().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
+    }
+}
