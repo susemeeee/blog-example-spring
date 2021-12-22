e05e89ec31d950e46b2af818af1a4ae3e74bbeaf
susemeeee <hffg123@naver.com>
2021-12-22 21:33:21 +0900

    Init example-spring-security-jwt

---
+++ example-spring-security-jwt/src/test/java/com/example/examplespringsecurityjwt/ExampleSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examplespringsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
